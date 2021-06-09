69cc72de9ae7eea5446ed1719ce7fd6f2b5ec2aa
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2021-06-09 21:53:44 +0300

    Add project files.

---
+++ Korelskiy/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Korelskiy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetBackgroundImage();
+        }
+        private void SetBackgroundImage()
+        {
+            int randomPhoto = new Random().Next(0, 4);
+            mainMenuBackgroundImage.Source = new BitmapImage(new Uri($"..\\Images\\MainMenuBacground\\{randomPhoto}"));
+        }
+    }
+}
